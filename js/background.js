f65e145f6729419a7ace2f68d2f5831beb6bfc28
ChaeyeonYang <myday0827@naver.com>
2021-12-10 00:47:09 +0900

    final

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
-const images = ["0.jpg","1.jpg","2.jpg"];
+const images = ["0.jpg", "1.jpeg", "2.jpeg", "3.jpg"];
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+const BG = "background";
 const bgImage = document.createElement("img");
 
-bgImage.src=`img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
+bgImage.className = BG;
 
 document.body.appendChild(bgImage);
